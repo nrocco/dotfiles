67eb247f6b5582edbc7f7f7bdc76c01ed3c00092
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-07-07 21:41:47 +0200

    Add host specific zsh configuration

---
+++ hosts/dev-wks/local.zsh
@@ -0,0 +1,12 @@
+if which tmux &> /dev/null
+then
+    if [[ -z "$TMUX" ]]
+    then
+        if ! tmux has-session -t default 2>/dev/null
+        then
+            tmux new-session -d -s default
+        fi
+
+        exec tmux attach-session -t default
+    fi
+fi
