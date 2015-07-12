42c63f0c046b39880c9555c631228d36abecefd8
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-07-12 19:31:33 +0200

    Fix ssh-agent socket in tmux.

--- hosts/dev-wks/local.zsh
+++ hosts/dev-wks/local.zsh
@@ -1,3 +1,10 @@
+if [ -S "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ]
+then
+    unlink "$HOME/.ssh/agent_sock" 2>/dev/null
+    ln -s "$SSH_AUTH_SOCK" "$HOME/.ssh/agent_sock"
+    export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
+fi
+
 if which tmux &> /dev/null
 then
     if [[ -z "$TMUX" ]]
--- hosts/vlan25-nro/local.zsh
+++ hosts/vlan25-nro/local.zsh
@@ -1,5 +1,12 @@
 alias tail_all='tail -f app/logs/* /var/log/apache2/*.log'
 
+if [ -S "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ]
+then
+    unlink "$HOME/.ssh/agent_sock" 2>/dev/null
+    ln -s "$SSH_AUTH_SOCK" "$HOME/.ssh/agent_sock"
+    export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
+fi
+
 if which tmux &> /dev/null
 then
     if [[ -z "$TMUX" ]]
