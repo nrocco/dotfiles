d9242113f88b9cb04436dd5e88fd288dbc45dd2e
Nico Di Rocco <dirocco.nico@gmail.com>
2015-10-24 10:17:22 +0200

    Make jq alias and json function available in general.

--- general.zsh
+++ general.zsh
@@ -19,7 +19,19 @@ alias drop_caches='sudo su -c "sync; echo 3 > /proc/sys/vm/drop_caches"'
 
 alias vim='vim -p'
 alias vi='vim'
-alias jq='jq --raw-output'
+
+if which jq > /dev/null
+then
+    alias jq='jq --raw-output'
+
+    function json {
+        if [ $# -eq 0 ]
+        then
+            set -- "."
+        fi
+        jq "$@"
+    }
+fi
 
 zstyle ':vcs_info:*' stagedstr ':+'
 zstyle ':vcs_info:*' unstagedstr ':*'
--- hosts/macbook.local/local.zsh
+++ hosts/macbook.local/local.zsh
@@ -1,15 +1,4 @@
-function json {
-    if [ $# -eq 0 ]
-    then
-        set -- "."
-    fi
-    jq --raw-output "$@"
-}
-
-alias vim='vim -p'
-
 autoload -Uz _notsio
-autoload -Uz _consume
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
