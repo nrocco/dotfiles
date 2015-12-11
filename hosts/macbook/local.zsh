55baa7698fbcde37a3761da30ae7fb0a6f696fd5
Nico Di Rocco <dirocco.nico@gmail.com>
2015-12-11 20:33:34 +0100

    Start gpg-agent.

--- hosts/macbook/local.zsh
+++ hosts/macbook/local.zsh
@@ -2,3 +2,13 @@ autoload -Uz _notsio
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
+
+local envfile="$HOME/.gnupg/gpg-agent.env"
+
+if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+    eval "$(cat "$envfile")"
+else
+    eval "$(gpg-agent --daemon --write-env-file "$envfile")"
+fi
+
+export GPG_AGENT_INFO  # the env file does not contain the export statement
