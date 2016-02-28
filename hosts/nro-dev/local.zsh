4746aaa2c2c6a8e9b2b4a9e7a890cc1632eba038
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2016-02-28 13:27:51 +0000

    Added a new host

---
+++ hosts/nro-dev/local.zsh
@@ -0,0 +1,26 @@
+alias tail_all='tail -f app/logs/* /var/log/apache2/*.log'
+alias ack=ack-grep
+
+if [ -S "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ]
+then
+    unlink "$HOME/.ssh/agent_sock" 2>/dev/null
+    ln -s "$SSH_AUTH_SOCK" "$HOME/.ssh/agent_sock"
+    export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
+fi
+
+precmd () {
+    printf "\033]2;`[[ ${PWD} == ${HOME} ]] && echo '~' || basename ${PWD}`\033\\"
+}
+
+if which tmux &> /dev/null
+then
+    if [[ -z "$TMUX" ]]
+    then
+        if ! tmux has-session -t default 2>/dev/null
+        then
+            tmux new-session -d -s default
+        fi
+
+        exec tmux attach-session -t default
+    fi
+fi
