aee05d170478f55d95505bf25372b82ad4173693
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2016-12-12 22:08:02 +0100

    Added new alias for macbooks

--- hosts/lsw-lpt765/local.zsh
+++ hosts/lsw-lpt765/local.zsh
@@ -4,6 +4,7 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 alias gateway="netstat -rnfinet | awk '/^default/ {print \$2}'"
+alias dnsclear="sudo killall -HUP mDNSResponder"
 
 local envfile="$HOME/.gnupg/gpg-agent.env"
 
