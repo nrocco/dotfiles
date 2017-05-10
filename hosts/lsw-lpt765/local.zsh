661b6ef0522aad27c8de10a2888a860cb2a7582c
Nico Di Rocco <dirocco.nico@gmail.com>
2017-05-10 14:23:02 +0200

    New gpg version has different settings

--- hosts/lsw-lpt765/local.zsh
+++ hosts/lsw-lpt765/local.zsh
@@ -3,17 +3,6 @@ export LANG=en_US.UTF-8
 
 alias dnsclear="sudo killall -HUP mDNSResponder"
 
-local envfile="$HOME/.gnupg/gpg-agent.env"
-
-if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
-    eval "$(cat "$envfile")"
-else
-    eval "$(gpg-agent --daemon --write-env-file "$envfile")"
-fi
-
-export GPG_AGENT_INFO  # the env file does not contain the export statement
-export GPG_TTY=`tty`
-
 function ssh {
     /usr/bin/ssh $@
 }
