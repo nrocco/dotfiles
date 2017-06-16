432736ef47a0203e6b4f0afc4d8f034d16597b87
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2017-06-16 18:45:02 +0000

    Add direnv

--- hosts/nro/local.zsh
+++ hosts/nro/local.zsh
@@ -1,7 +1,4 @@
-alias ll='ls -lhe'
-alias grep='grep'
 alias vagrant='VAGRANT_DEFAULT_PROVIDER=cloudstack vagrant'
-
 
 if [ -S "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ]
 then
@@ -13,6 +10,8 @@ fi
 precmd () {
     tmux rename-window "${PWD##/*/}"
 }
+
+eval "$(direnv hook zsh)"
 
 if which tmux &> /dev/null
 then
