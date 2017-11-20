47b86c5a1136ad49e01e7c45e45fe74b02065be0
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2017-11-20 21:50:02 +0000

    Run tmux in utf-8 mode

--- hosts/nro/local.zsh
+++ hosts/nro/local.zsh
@@ -22,11 +22,11 @@ then
 
     if [[ -z "$TMUX" ]]
     then
-        if ! tmux has-session -t default 2>/dev/null
+        if ! tmux -u has-session -t default 2>/dev/null
         then
-            tmux new-session -d -s default
+            tmux -u new-session -d -s default
         fi
 
-        exec tmux attach-session -t default
+        exec tmux -u attach-session -t default
     fi
 fi
