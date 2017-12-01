e44bbb325d3f8b8e056d4216105f5bee7210a58a
Nico Di Rocco <dirocco.nico@gmail.com>
2017-12-01 19:10:06 +0100

    Also add /usr/local/sbin to path if homebrew is detected

--- zsh/framework/00_OS-Darwin.zsh
+++ zsh/framework/00_OS-Darwin.zsh
@@ -4,6 +4,7 @@ fi
 
 if which brew > /dev/null
 then
+    export PATH="/usr/local/sbin:${PATH}"
     if [ ! -f "${ZSH_CACHE}/coreutils_path" ]
     then
         brew --prefix coreutils > "${ZSH_CACHE}/coreutils_path"
@@ -13,8 +14,8 @@ fi
 
 if [ -d "${BREW_COREUTILS}" ]; then
     # let GNU coreutils installed by brew take preecedence on Mac systems
-    export PATH="${BREW_COREUTILS}/libexec/gnubin:$PATH"
-    export MANPATH="${BREW_COREUTILS}/libexec/gnuman:$MANPATH"
+    export PATH="${BREW_COREUTILS}/libexec/gnubin:${PATH}"
+    export MANPATH="${BREW_COREUTILS}/libexec/gnuman:${MANPATH}"
 else
     # the OSX way for ls colors.
     export CLICOLOR=1
