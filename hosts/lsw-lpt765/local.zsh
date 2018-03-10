b98db819162e5eab3a93a55af24ee520e24075f9
Nico Di Rocco <dirocco.nico@gmail.com>
2018-03-10 09:57:32 +0100

    Add ~/bin to path

--- hosts/lsw-lpt765/local.zsh
+++ hosts/lsw-lpt765/local.zsh
@@ -1,3 +1,4 @@
+export PATH=~/bin:$PATH
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export GOPATH=~/go
@@ -10,17 +11,17 @@ alias lswcli='python3 -m lswcli'
 function chefgrep {
     grep -R --exclude-dir=.git "$@" /Users/nrocco/Develop/chef-repo/cookbooks
 }
-compdef _grep chefgrep=grep
+compdef chefgrep=grep
 
 function wireshark-remote {
     ssh "$@" "tcpdump -s0 -U -n -w - 'not port 22'" | wireshark -k -i -
 }
-compdef _ssh wireshark-remote=ssh
+compdef wireshark-remote=ssh
 
 function wireshark-remote-sudo {
     ssh "$@" "sudo tcpdump -s0 -U -n -w - 'not port 22'" | wireshark -k -i -
 }
-compdef _ssh wireshark-remote-sudo=ssh
+compdef wireshark-remote-sudo=ssh
 
 function livereplica {
     curl --insecure -H 'Host: api.leaseweb.com' -H 'Content-Type: application/json' -H "Authorization: Bearer ${TOKEN}" https://10.11.11.98$@ | jq
