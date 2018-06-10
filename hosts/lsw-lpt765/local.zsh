0c2f50c597253b90fba102d9a7c57c855de00dbc
Nico Di Rocco <dirocco.nico@gmail.com>
2018-06-10 09:21:39 +0200

    Update location of fzf completion and key-binding files

--- hosts/lsw-lpt765/local.zsh
+++ hosts/lsw-lpt765/local.zsh
@@ -4,8 +4,8 @@ export GOPATH=~/go
 export DOCKER_HIDE_LEGACY_COMMANDS=yes
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
-source "/usr/local/Cellar/fzf/0.17.3/shell/completion.zsh"
-source "/usr/local/Cellar/fzf/0.17.3/shell/key-bindings.zsh"
+source "/usr/local/Cellar/fzf/0.17.4/shell/completion.zsh"
+source "/usr/local/Cellar/fzf/0.17.4/shell/key-bindings.zsh"
 
 alias dnsclear="sudo killall -HUP mDNSResponder"
 alias lswcli='python3 -m lswcli'
@@ -35,3 +35,4 @@ compdef wireshark-remote-sudo=ssh
 alias appearin='open -n -a Chromium --args --no-default-browser-check --disable-sync http://appear.in/bare-mentals'
 alias vlan11='open -n -a Chromium --args --user-data-dir="/Users/nrocco/Library/Application Support/ChromiumVlan11" --no-default-browser-check --ignore-certificate-errors --disable-sync --proxy-server="socks://10.11.11.3:1080"'
 alias vlan33='open -n -a Chromium --args --user-data-dir="/Users/nrocco/Library/Application Support/ChromiumVlan33" --no-default-browser-check --ignore-certificate-errors --disable-sync --proxy-server="socks://10.11.33.3:1080"'
+alias vpn='gpg --decrypt ~/.openconnect-password | sudo openconnect --config ~/.openconnect.conf connect.ocom.com'
