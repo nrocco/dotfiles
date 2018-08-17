884af51c21c01fdea778ad4b69ce1fac4a30d882
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2018-08-17 22:20:24 +0200

    Configure fzf for vim

--- zsh/framework/98_Local.zsh
+++ zsh/framework/98_Local.zsh
@@ -1,4 +1,4 @@
-export FZF_DEFAULT_COMMAND='rg --files --color never --no-ignore --hidden --follow --glob "!.git/*" --glob "!build/coverage/*" --glob "!app/logs/*" --glob "!app/cache/*"'
+export FZF_DEFAULT_COMMAND='rg --files --color never --no-ignore --hidden --follow --glob "!.git/*" --glob "!build/coverage/*" --glob "!app/logs/*" --glob "!app/cache/*" --glob "!var/*"'
 export DOCKER_HIDE_LEGACY_COMMANDS=yes
 
 compdef rgit=git
