213148424d974fab084500c6a60958b22308ab42
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2018-08-17 20:43:58 +0200

    New host specific file

--- hosts/lsw-lpt879/local.zsh
+++ hosts/lsw-lpt879/local.zsh
@@ -1,6 +1,6 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-export GOPATH=~/go
+export GOPATH=/usr/local/go
 
 source "/usr/local/Cellar/fzf/0.17.4/shell/completion.zsh"
 source "/usr/local/Cellar/fzf/0.17.4/shell/key-bindings.zsh"
@@ -14,7 +14,6 @@ path=(
     "${HOME}/bin"
     $path
     "${HOME}/.composer/vendor/bin"
-    "${GOPATH}/bin"
 )
 
 function chefgrep {
@@ -32,7 +31,4 @@ function wireshark-remote-sudo {
 }
 compdef wireshark-remote-sudo=ssh
 
-alias appearin='open -n -a Chromium --args --no-default-browser-check --disable-sync http://appear.in/bare-mentals'
-alias vlan11='open -n -a Chromium --args --user-data-dir="/Users/nrocco/Library/Application Support/ChromiumVlan11" --no-default-browser-check --ignore-certificate-errors --disable-sync --proxy-server="socks://10.11.11.3:1080"'
-alias vlan33='open -n -a Chromium --args --user-data-dir="/Users/nrocco/Library/Application Support/ChromiumVlan33" --no-default-browser-check --ignore-certificate-errors --disable-sync --proxy-server="socks://10.11.33.3:1080"'
 alias vpn='gpg --decrypt ~/.openconnect-password | sudo openconnect --config ~/.openconnect.conf connect.ocom.com'
