5ad1792828465702ac3458f498f1421442309747
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2018-10-04 19:49:35 +0200

    Remove Report timing statistics for commands

--- zsh/framework/02_Environment.zsh
+++ zsh/framework/02_Environment.zsh
@@ -5,10 +5,6 @@ export VISUAL=vim
 # Allows to kill backward word path by path using ctrl+w
 export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 
-# Report timing statistics for commands that take more than this, in seconds
-export REPORTTIME=3
-export TIMEFMT="time: %U user %S system %P cpu %*E total  %J"
-
 if which dircolors > /dev/null
 then
     if [ -f "${0:h}/../nord-dircolors/src/dir_colors" ]
