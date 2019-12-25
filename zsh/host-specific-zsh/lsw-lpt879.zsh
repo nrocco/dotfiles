21991214505edaef4e69c7f01ff9fe9419e25821
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2019-12-25 14:25:47 +0100

    Add uncommited zsh configuration to git

--- zsh/host-specific-zsh/lsw-lpt879.zsh
+++ zsh/host-specific-zsh/lsw-lpt879.zsh
@@ -1,7 +1,27 @@
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-
 export GOPATH=/usr/local/go
+
+alias virt-install='docker container run --rm --interactive --tty --env "LIBVIRT_DEFAULT_URI=$LIBVIRT_DEFAULT_URI" nrocco/virt-install:latest'
+
+function vm() {
+    if [[ -z $1 || -z $2 ]]
+    then
+        echo vm [name] [image]
+        return 1
+    fi
+
+    virt-install \
+        --name "${1}" \
+        --ram 1024 \
+        --import \
+        --os-type generic \
+        --network network=default,model=virtio \
+        --boot hd \
+        --graphics vnc \
+        --noautoconsole \
+        --disk "pool=default,size=50,backing_store=${2},backing_format=qcow2,format=qcow2"
+}
 
 source "/usr/local/var/homebrew/linked/fzf/shell/completion.zsh"
 source "/usr/local/var/homebrew/linked/fzf/shell/key-bindings.zsh"
@@ -11,7 +31,20 @@ alias mtr='sudo /usr/local/sbin/mtr'
 alias ping='sudo /sbin/ping'
 alias traceroute='sudo /usr/sbin/traceroute'
 
-path=("${HOME}/bin" ${path} "${HOME}/.composer/vendor/bin" "${GOPATH}/bin")
+if [ -d "${HOME}/.composer/vendor/bin" ]
+then
+    path=($path "${HOME}/.composer/vendor/bin")
+fi
+
+if [ -d "${GOPATH}/bin" ]
+then
+    path=($path "${GOPATH}/bin")
+fi
+
+if [ -d "${HOME}/bin" ]
+then
+    path=("${HOME}/bin" $path)
+fi
 
 compdef wireshark-remote=ssh
 
