67b0dea2eb87ea46328460578843f25188f1a8e1
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2020-03-20 20:21:19 +0100

    Add aliases to zshrc

--- zsh/host-specific-zsh/lsw-lpt879.zsh
+++ zsh/host-specific-zsh/lsw-lpt879.zsh
@@ -2,14 +2,24 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export GOPATH=/usr/local/go
 
-alias virt-install='docker container run --rm --interactive --tty --env "LIBVIRT_DEFAULT_URI=$LIBVIRT_DEFAULT_URI" --entrypoint= nrocco/virt-install:latest /app/virt-install'
+alias virt-install='docker container run --rm --interactive --tty --env "LIBVIRT_DEFAULT_URI=$LIBVIRT_DEFAULT_URI" nrocco/virt-install:latest'
+alias convert='docker container run --rm -v "$PWD:$PWD" -w "$PWD" nrocco/imagemagick:latest convert'
+alias mogrify='docker container run --rm -v "$PWD:$PWD" -w "$PWD" nrocco/imagemagick:latest mogrify'
 
+# Convert a date to a UTC timestamp
 function to_timestamp() {
     date -d "$1" -u +"%s"
 }
 
+# Convert a timestamp to a UTC date
 function to_date() {
     date -d "@$1" -u
+}
+
+# Prepare a multiline string for use in a chef databag
+function key_for_data_bag() {
+    cat "$1" | sed s/$/\\\\n/ | tr -d '\n'
+    echo
 }
 
 function vm() {
