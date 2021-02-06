515971f847de8a6dbe61b696a786ab4c41cedddd
Nico Di Rocco <dirocco.nico@gmail.com>
2021-02-06 15:05:01 +0100

    Make the lint_whitespace configurable

--- bin/fuubar.py
+++ bin/fuubar.py
@@ -144,36 +144,17 @@ def lint_foodcritic(file):
             }
 
 
-def lint_whitespace(file):
+def lint_whitespace(file, spaces=True, clrf=True, tabs=True):
     linenr = 1
     with open(file, 'rb') as in_file:
         for line in in_file:
-            if re.match(b"^.* (\r)?\n$", line):
+            if spaces and re.match(b"^.*[ \t](\r)?\n$", line):
                 yield {'file': file, 'line': linenr, 'message': 'trailing spaces detected'}
-            if re.match(b"^.*\r\n$", line):
+            if clrf and re.match(b"^.*\r\n$", line):
                 yield {'file': file, 'line': linenr, 'message': 'windows line endings detected'}
-            if re.match(b"^.*\t.*$", line):
+            if tabs and re.match(b"^\t+", line):
                 yield {'file': file, 'line': linenr, 'message': 'literal tab characters detected'}
             linenr += 1
-
-
-def lint(args):
-    mapping = {
-        '.go':   [lint_whitespace, lint_govet, lint_golint],
-        '.json': [lint_whitespace, lint_jq],
-        '.php':  [lint_whitespace, lint_php, lint_phpstan],
-        '.py':   [lint_whitespace, lint_flake8],
-        '.rb':   [lint_whitespace, lint_rubocop, lint_foodcritic],
-        '.yaml': [lint_whitespace, lint_yaml],
-        '.yml':  [lint_whitespace, lint_yaml],
-    }
-    for file in args.files:
-        for ext, linters in mapping.items():
-            if file.endswith(ext):
-                for linter in linters:
-                    # print("==> Run {} on file {}".format(linter.__name__, file))
-                    for error in linter(file):
-                        print("{file}||{line}||{message}".format(**error))
 
 
 def fix_clrf(file):
@@ -189,6 +170,14 @@ def fix_php_cs_fixer(file):
     subprocess.run(['php-cs-fixer', 'fix', '--no-ansi', '--no-interaction', file], check=True)
 
 
+def fix_gofmt(file):
+    subprocess.run(['gofmt', '-w', '-s', file], check=True)
+
+
+def fix_goimports(file):
+    subprocess.run(['goimports', '-w', file], check=True)
+
+
 def fix_json(file):
     with open(file, 'r') as in_file:
         data = json.load(in_file)
@@ -197,25 +186,54 @@ def fix_json(file):
         out_file.write('\n')
 
 
-def fix(args):
-    mapping = {
-        '.go':   [fix_clrf, fix_whitespace],
-        '.json': [fix_clrf, fix_whitespace, fix_json],
-        '.php':  [fix_clrf, fix_whitespace, fix_php_cs_fixer],
-        '.rb':   [fix_clrf, fix_whitespace],
-        '.yaml': [fix_clrf, fix_whitespace],
-        '.yml':  [fix_clrf, fix_whitespace],
-    }
+def lint(args):
     for file in args.files:
-        for ext, fixers in mapping.items():
+        for ext, linters in MAPPING['linter'].items():
+            if file.endswith(ext):
+                for linter in linters:
+                    if type(linter) is list:
+                        linter, kwargs = linter
+                    else:
+                        kwargs = {}
+                    if args.verbose:
+                        print("==> Run {} on file {}".format(linter.__name__, file))
+                    for error in linter(file, **kwargs):
+                        print("{file}||{line}||{message}".format(**error))
+
+
+def fix(args):
+    for file in args.files:
+        for ext, fixers in MAPPING['fixer'].items():
             if file.endswith(ext):
                 for fixer in fixers:
                     print("==> Run {} on file {}".format(fixer.__name__, file))
                     fixer(file)
 
 
+MAPPING = {
+    'linter': {
+        '.go':   [[lint_whitespace, dict(tabs=False)], lint_govet, lint_golint],
+        '.json': [lint_whitespace, lint_jq],
+        '.php':  [lint_whitespace, lint_php, lint_phpstan],
+        '.py':   [lint_whitespace, lint_flake8],
+        '.rb':   [lint_whitespace, lint_rubocop, lint_foodcritic],
+        '.yaml': [lint_whitespace, lint_yaml],
+        '.yml':  [lint_whitespace, lint_yaml],
+    },
+    'fixer': {
+        '.go':   [fix_clrf, fix_whitespace, fix_gofmt, fix_goimports],
+        '.json': [fix_clrf, fix_whitespace, fix_json],
+        '.php':  [fix_clrf, fix_whitespace, fix_php_cs_fixer],
+        '.rb':   [fix_clrf, fix_whitespace],
+        '.yaml': [fix_clrf, fix_whitespace],
+        '.yml':  [fix_clrf, fix_whitespace],
+    }
+}
+
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", action='store_true', help="show what is being done")
     parser.add_argument("files", nargs='*', help="files to lint")
     args = parser.parse_args()
     if sys.argv[0].endswith('linter'):
