98616d052c84e6a7514ffdf46de9c44d8065a798
Nico Di Rocco <dirocco.nico@gmail.com>
2021-05-31 18:17:37 +0000

    Improved combination of ssh-agent and tmux
    See https://werat.dev/blog/happy-ssh-agent-forwarding/

--- zsh/host-specific-zsh/nro.zsh
+++ zsh/host-specific-zsh/nro.zsh
@@ -16,17 +16,10 @@ then
     path=("${HOME}/bin" $path)
 fi
 
-function plgrep {
+function lswrg {
     rg --ignore --ignore-vcs "$@" -- ~/nro/bundles/* ~/nro/frontend-layer/* ~/nro/process-layer/* ~/nro/python/* ~/nro/service-layer/*
 }
-compdef plgrep=rg
-
-if [ -S "$SSH_AUTH_SOCK" -a "$SSH_AUTH_SOCK" != "$HOME/.ssh/agent_sock" ]
-then
-    unlink "$HOME/.ssh/agent_sock" 2>/dev/null
-    ln -s "$SSH_AUTH_SOCK" "$HOME/.ssh/agent_sock"
-    export SSH_AUTH_SOCK="$HOME/.ssh/agent_sock"
-fi
+compdef lswrg=rg
 
 # Fix key bindings in tmux
 bindkey "\e[1~" beginning-of-line
